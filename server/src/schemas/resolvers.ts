13ee30999b92a6349a6f36ff031f3f8d4115ad39
Zach <zowen17@gmail.com>
2025-03-31 18:31:19 -0400

    updated index ts in models and in the schema folder and resolvers

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,5 +1,5 @@
-import { User, Book } from '../models/User';
-import { signtoken, Authentication } from '../services/auth';
+import { User, Book } from '../models/index';
+import { signToken, authenticateToken } from '../services/auth';
 
 interface AddUserArgs {
     input: {
@@ -14,7 +14,7 @@ interface LoginArgs {
     password: string;
 }
 
-interface SaveBookArgs {
+interface SaveBook {
     bookId: string;
     authors: string[];
     description: string;
@@ -27,56 +27,88 @@ interface RemoveBookArgs {
     bookId: string;
 }
 
-interface AddBookArgs {
-    bookId: string;
-    authors: string[];
-    description: string;
-    title: string;
-    image: string;
-    link: string;
-}
+// interface AddBookArgs {
+//     bookId: string;
+//     authors: string[];
+//     description: string;
+//     title: string;
+//     image: string;
+//     link: string;
+// }
 
-const resolvers = {
+import { IResolvers } from '@graphql-tools/utils';
+
+const resolvers: IResolvers = {
     Query: {
-        user: async (parent: any, args: { username: string }) => {
+        user: async (args: { username: string }) => {
             const user = await User.findOne({ username: args.username }).populate('savedBooks');
             return user;
         }
     },
-}
 
-const Mutation = {
-    addUser: async (parent: any, { input }: AddUserArgs) => {
-        const user = await User.create(input);
-        const token = signtoken({ username: user.username, email: user.email });
-        return { token, user };
-    },
-    login: async (parent: any, { email, password }: LoginArgs) => {
-        const user = await User.findOne({ email });
-        if (!user) {
-            throw new Error('No user found with this email address');
-        }
-        const isCorrectPassword = await user.isCorrectPassword(password);
-        if (!isCorrectPassword) {
-            throw new Error('Incorrect password');
-        }
-        const token = signtoken({ username: user.username, email: user.email });
-        return { token, user };
-    },
-    saveBook: async (parent: any, { bookId, authors, description, title, image, link }: SaveBookArgs) => {
-        const book = await Book.create({ bookId, authors, description, title, image, link });
-        return book;
-    },
-    removeBook: async (parent: any, { bookId }: RemoveBookArgs) => {
-        const book = await Book.findByIdAndDelete(bookId);
-        return book;
+    Mutation: {
+        addUser: async ({ input }: AddUserArgs) => {
+            try {
+                const user = await User.create(input);
+                const token = signToken(user.username, user._id); // Avoid including the password
+                return { token, user };
+            } catch (error) {
+                throw new Error('Error creating user: ');
+            }
+        },
+        login: async ({ email, password }: LoginArgs): Promise<any> => {
+            try {
+                const user = await User.findOne({ email });
+                if (!user) {
+                    throw new Error('No user found with this email address');
+                }
+                const isCorrectPassword = await user.isCorrectPassword(password);
+                if (!isCorrectPassword) {
+                    throw new Error('Incorrect password');
+                }
+                const token = signToken(user.username, user._id); // Avoid including the password
+                return { token, user };
+            } catch (error) {
+                throw new Error('Error logging in: ');
+            }
+        },
+        saveBook: async (args: SaveBook, context: any) => {
+            if (!context.user) {
+                throw new Error('You need to be logged in to save a book');
+            }
+            const { bookId, authors, description, title, image, link } = args;
+            try {
+                const book = await Book.create({ bookId, authors, description, title, image, link });
+                return book;
+            } catch (error) {
+                throw new Error('Error saving book: ');
+            }
+        },
+        removeBook: async ({ bookId }: RemoveBookArgs, context: any) => {
+            if (!context.user) {
+                throw new Error('You need to be logged in to remove a book');
+            }
+            try {
+                const book = await Book.findByIdAndDelete(bookId);
+                return book;
+            } catch (error) {
+                throw new Error('Error removing book: ');
+            }
+        },
     },
 };
-const authMiddleware = async (parent: any, args: any, context: any) => {
+
+const authMiddleware = async (context: any, next: Function) => {
     const token = context.req.headers.authorization || '';
     if (token) {
-        const user = await Authentication(token);
-        context.user = user;
+        try {
+            const user = await authenticateToken(token);
+            context.user = user;
+        } catch (error) {
+            throw new Error('Authentication failed: ');
+        }
     }
     return next();
-}
+};
+
+export { resolvers, authMiddleware };
